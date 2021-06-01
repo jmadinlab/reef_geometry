b8d50ff9c37ae1b000a0631b11974aee14f69f92
Joshua Madin <jmadin@hawaii.edu>
2021-06-01 09:08:51 -1000

    Final annotations

--- R/coral_annotation_shp.R
+++ R/coral_annotation_shp.R
@@ -13,16 +13,16 @@ S2a <- readOGR("data/coral_annotations/S2/S2_corals_anna.shp")
 S2a@data <- S2a@data["species"]
 S2b <- readOGR("data/coral_annotations/S2/S2_corals_annie.shp")
 S2b@data <- S2b@data["species"]
-# S2c <- readOGR("data/coral_annotations/S2/S2_SF.shp")
-# S2c@data <- S2c@data["species"]
-S2 <- bind(S2a, S2b)
+S2c <- readOGR("data/coral_annotations/S2/S2_Corals_Fiza .shp")
+S2c@data <- S2c@data["species"]
+S2 <- bind(S2a, S2b, S2c)
 writeOGR(S2, "output/coral_annotations", layer="S2", driver="ESRI Shapefile", overwrite_layer=TRUE)
 
 S3a <- readOGR("data/coral_annotations/S3/S3_corals_rago.shp")
 S3a@data <- S3a@data["species"]
 S3b <- readOGR("data/coral_annotations/S3/S3_corals_whit.shp")
 S3b@data <- S3b@data["species"]
-# S3c <- readOGR("data/coral_annotations/S3/S3_SF.shp")
-# S3c@data <- S3c@data["species"]
-S3 <- bind(S3a, S3b)
+S3c <- readOGR("data/coral_annotations/S3/S3_corals_Nic.shp")
+S3c@data <- S3c@data["species"]
+S3 <- bind(S3a, S3b, S3c)
 writeOGR(S3, "output/coral_annotations", layer="S3", driver="ESRI Shapefile", overwrite_layer=TRUE)
